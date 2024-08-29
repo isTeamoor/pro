21a3514a8a15eebe77e11fa69fbe418ab623196e
Is Teamoor <is.teamoor@gmail.com>
2024-08-29 17:12:29 +0500

    29.08.2024

--- src/components/Screens/tests.vue
+++ src/components/Screens/tests.vue
@@ -1,9 +1,24 @@
 <template>
-    <h1>tst</h1>
+    <div class="test">
+        <div class="subtest"></div>
+    </div>
 </template>
 <script>
 export default {
     name:'test'
 }
 </script>
-<style></style>
+<style scoped>
+.subtest {
+    display: inline-block;
+    width: 300px;
+    height: 200px;
+    border: 1px solid blue;
+    background: yellow;
+    background-image: repeating-linear-gradient(to right, black, black 15px, transparent 30px);
+
+
+    
+    
+}
+</style>
